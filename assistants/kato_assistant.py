69f9674fb632b4addeac2283d13638ccd1a55b57
Katherine Perez <87502202+ktperez@users.noreply.github.com>
2024-03-26 11:33:54 -0500

    Delete assistants directory

--- assistants/kato_assistant.py
+++
@@ -1,5 +0,0 @@
-class KatoAssistant:
-    def get_response(self, user_query):
-        # Lógica para procesar la consulta y generar una respuesta
-        response = "Hola! Veo que estás interesado en productos capilares. ¿En qué puedo ayudarte?"
-        return response
